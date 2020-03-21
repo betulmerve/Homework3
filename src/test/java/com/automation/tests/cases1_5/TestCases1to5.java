bd96f2d57a75e8d6ccef9876cc0d22fed21755a9
betulmerve <merve.faki@gmail.com>
2020-03-20 21:15:14 -0400

    commit

--- src/test/java/com/automation/tests/cases1_5/TestCases1to5.java
+++ src/test/java/com/automation/tests/cases1_5/TestCases1to5.java
@@ -1,11 +1,34 @@
 package com.automation.tests.cases1_5;
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+
 
 public class TestCases1to5 {
 
-
     private WebDriver driver;
+
+    @Test
+    public void testCase1() throws Exception{
+        driver.get("https://practice-cybertekschool.herokuapp.com");
+        Thread.sleep(3000);
+    }
+
+
+
+    @BeforeMethod
+    public void setup(){
+        WebDriverManager.chromedriver().version("79").setup();
+        driver=new ChromeDriver();
+    }
+
+    @AfterMethod
+    public void tearDown(){
+        driver.quit();
+    }
 
 
 }
