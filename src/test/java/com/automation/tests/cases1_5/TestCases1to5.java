1af013f015baf57665cacc1608f55e9366c80851
betulmerve <merve.faki@gmail.com>
2020-03-20 20:35:58 -0400

    first commit

--- src/test/java/com/automation/tests/cases1_5/TestCases1to5.java
+++ src/test/java/com/automation/tests/cases1_5/TestCases1to5.java
@@ -1,9 +1,12 @@
 package com.automation.tests.cases1_5;
+import org.openqa.selenium.WebDriver;
 import org.testng.annotations.Test;
 
 
 public class TestCases1to5 {
 
+
+    private WebDriver driver;
 
 
 }
