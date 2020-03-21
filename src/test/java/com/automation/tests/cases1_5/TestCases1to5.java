ad0aa6d3cf5f17c15e0006872508f998d25c9b79
betulmerve <merve.faki@gmail.com>
2020-03-20 21:41:20 -0400

    commit

--- src/test/java/com/automation/tests/cases1_5/TestCases1to5.java
+++ src/test/java/com/automation/tests/cases1_5/TestCases1to5.java
@@ -1,5 +1,6 @@
 package com.automation.tests.cases1_5;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
@@ -15,8 +16,15 @@ public class TestCases1to5 {
     public void testCase1() throws Exception{
         driver.get("https://practice-cybertekschool.herokuapp.com");
         Thread.sleep(3000);
-    }
+        driver.findElement(By.linkText("Registration Form")).click();
+        Thread.sleep(3000);
+        driver.findElement(By.name("birthday")).sendKeys("wrong_dob");
+        Thread.sleep(3000);
 
+        String expected="";
+        String actual="";
+
+    }
 
 
     @BeforeMethod
