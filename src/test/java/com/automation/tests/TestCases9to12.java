dc100cb873c6485467c9dc249b2c8301672d07a9
betulmerve <merve.faki@gmail.com>
2020-03-23 23:06:05 -0400

    v2

--- src/test/java/com/automation/tests/TestCases9to12.java
+++ src/test/java/com/automation/tests/TestCases9to12.java
@@ -9,50 +9,56 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 public class TestCases9to12 {
 
     private WebDriver driver;
-    @Test
-    public void testCase9(){
-        //step 4?
 
-        driver.findElement(By.linkText("200")).click();
-        BrowserUtils.wait(2);
-        String expected="This page returned a 200 status code";
-        WebElement message=driver.findElement(By.tagName("p"));
-        Assert.assertTrue(message.isDisplayed());
-    }
-    @Test
-    public void testCase10(){
-
-        driver.findElement(By.linkText("301")).click();
-        BrowserUtils.wait(2);
-        String expected="This page returned a 301 status code";
-        WebElement message=driver.findElement(By.tagName("p"));
-        Assert.assertTrue(message.isDisplayed());
+    @DataProvider(name = "testData")
+    public static Object[] testData(){
+        return new Object[]{"200","301","404","500"};
     }
 
-    @Test
-    public void testCase11(){
+    @Test(dataProvider = "testData")
+    public void testCase9(String code){
 
-        driver.findElement(By.linkText("404")).click();
+        driver.findElement(By.linkText(code)).click();
         BrowserUtils.wait(2);
-        String expected="This page returned a 404 status code";
+        String expected="This page returned a "+code+" status code";
         WebElement message=driver.findElement(By.tagName("p"));
-        Assert.assertTrue(message.isDisplayed());
+        Assert.assertTrue(message.getText().contains(expected),"The status code does not exist");
     }
-
-    @Test
-    public void testCase12(){
-
-        driver.findElement(By.linkText("500")).click();
-        BrowserUtils.wait(2);
-        String expected="This page returned a 500 status code";
-        WebElement message=driver.findElement(By.tagName("p"));
-        Assert.assertTrue(message.isDisplayed());
-    }
+//    @Test(dataProvider = "testData")
+//    public void testCase10(String code){
+//
+//        driver.findElement(By.linkText(code)).click();
+//        BrowserUtils.wait(2);
+//        String expected="This page returned a "+code+" status code";
+//        WebElement message=driver.findElement(By.tagName("p"));
+//        Assert.assertTrue(message.isDisplayed());
+//    }
+//
+//    @Test(priority = 3,dataProvider = "testData")
+//    public void testCase11(String code){
+//
+//        driver.findElement(By.linkText(code)).click();
+//        BrowserUtils.wait(2);
+//        String expected="This page returned a "+code+" status code";
+//        WebElement message=driver.findElement(By.tagName("p"));
+//        Assert.assertTrue(message.isDisplayed());
+//    }
+//
+//    @Test(priority = 4,dataProvider = "testData")
+//    public void testCase12(String code){
+//
+//        driver.findElement(By.linkText(code)).click();
+//        BrowserUtils.wait(2);
+//        String expected="This page returned a "+code+" status code";
+//        WebElement message=driver.findElement(By.tagName("p"));
+//        Assert.assertTrue(message.isDisplayed());
+//    }
 
 
     @BeforeMethod
@@ -64,6 +70,7 @@ public class TestCases9to12 {
         driver.findElement(By.linkText("Status Codes")).click();
         BrowserUtils.wait(2);
     }
+
 
     @AfterMethod
     public void tearDown(){
